6ea8da88cfbded622ac5a8a9d9d81fcc645e016f
w1q <w1q@w1qs-MacBook-Air.local>
2021-07-13 00:14:53 +0300

    3

--- i2.js
+++ i2.js
@@ -1,5 +1,5 @@
 var numbers = [66, 34, 4424, 1, 8, 56, 22];
 numbers.sort(function(a, b) {
-  		return a - b;
+  		return a > b;
 });
 console.log(numbers); // [1, 2, 3, 4, 5, 6, 7]
---
+++ i4.js
@@ -0,0 +1,9 @@
+var inputStr = [5, 2, 1, -10, 8];
+
+function compare(a, b) {
+  return a - b;
+}
+
+inputStr.sort(compare);
+
+console.log(inputStr);
